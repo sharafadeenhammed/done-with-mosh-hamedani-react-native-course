ac596240222ee6d65359c7e1369877365c909a5c
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-09-13 16:37:48 +0100

    implemeted a requwst Transormere

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,16 @@
 import { create } from "apisauce";
 
 import cache from "../utility/cache";
+import AuthStorage from "../utility/AuthStorage";
 
 const apiClient = create({
   baseURL: "http://192.168.180.85:9000",
+});
+
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await AuthStorage.get();
+  if (!authToken) return;
+  request.headers["x-auth-token"] = authToken;
 });
 
 const get = apiClient.get;
